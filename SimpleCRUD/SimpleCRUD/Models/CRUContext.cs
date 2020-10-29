54a5e8b0600281ef91118a591c4b8161a7554375
Simanga Shabalala <simangae@gmail.com>
2020-10-29 13:33:18 +0200

    Initial Setup and EntityFramwork Setup, Added Model

---
+++ SimpleCRUD/SimpleCRUD/Models/CRUContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCRUD.Models
+{
+    public class CRUContext : DbContext
+    {
+        public CRUContext()
+            :base("SimpleCrudConnection")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
