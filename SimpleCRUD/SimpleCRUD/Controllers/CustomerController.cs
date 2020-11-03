76dd3b2ab41b0800c43c52f509908c7599b01c4e
Simanga Shabalala <simangae@gmail.com>
2020-11-03 13:08:30 +0200

    Added Controller And Index View

---
+++ SimpleCRUD/SimpleCRUD/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using SimpleCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCRUD.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CRUContext context = new CRUContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = context.Customers.ToList();
+            return View(customers);
+        }
+    }
+}
