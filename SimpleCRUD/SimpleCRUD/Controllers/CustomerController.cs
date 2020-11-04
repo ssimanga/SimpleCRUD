bd689ca81f65d4c69a51b09e287bf2c45df8663a
Simanga Shabalala <simangae@gmail.com>
2020-11-04 13:20:03 +0200

    Update mthod Addedd and working

--- SimpleCRUD/SimpleCRUD/Controllers/CustomerController.cs
+++ SimpleCRUD/SimpleCRUD/Controllers/CustomerController.cs
@@ -1,7 +1,9 @@
 ﻿using SimpleCRUD.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -33,6 +35,32 @@ namespace SimpleCRUD.Controllers
                 
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(string id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var customer = context.Customers.SingleOrDefault(c => c.Id == id);
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(customer).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
         }
     }
 }
